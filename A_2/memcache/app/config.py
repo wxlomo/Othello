ba3c6cd4dc5effbccccaf2b9bcc647d5d36e1471
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-07 20:22:35 -0500

    add cricidentials

---
+++ A_2/memcache/app/config.py
@@ -0,0 +1,14 @@
+db_config = {
+    'user': 'root',
+    'password': 'ece1779pass',
+    'host': '127.0.0.1',
+    'database': 'gallery'
+}
+awsKey={
+    'aws_access_key_id' : 'AKIA3NQ4GILKF3U7HZWT',
+    'aws_secret_access_key' : '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'
+}
+VPCID='vpc-042054f0f945d031c'
+SubnetID='subnet-0c43635379007a839'
+SecurityGroupID='sg-0bd84a8e573f6d497'
+instances={}
--- A_2/memcache/app/getcloudstat.py
+++ A_2/memcache/app/getcloudstat.py
@@ -2,14 +2,14 @@ import time
 import datetime
 from dateutil.tz import tzutc
 import boto3
-from . import cloudwatch_config, instances
+from . import awsKey, instances, VPCID, SubnetID, SecurityGroupID
 ami = "ami-080ff70d8f5b80ba5"
 
 def getAggregateMissRate1mins(instances: list, intervals=60, period=60):
     client = boto3.client('cloudwatch', 
                             region_name='us-east-1',
-                            aws_access_key_id=cloudwatch_config.aws_access_key_id,
-                            aws_secret_access_key=cloudwatch_config.aws_secret_access_key)
+                            aws_access_key_id=awsKey.aws_access_key_id,
+                            aws_secret_access_key=awsKey.aws_secret_access_key)
     startTime = datetime.datetime.utcnow() - datetime.timedelta(seconds=intervals)
     endTime = datetime.datetime.utcnow()
     miss = 0
@@ -55,8 +55,8 @@ def getAggregateStat30Mins(instances: list):
     hitRate=[]
     client = boto3.client('cloudwatch', 
                         region_name='us-east-1',
-                        aws_access_key_id=cloudwatch_config.aws_access_key_id,
-                        aws_secret_access_key=cloudwatch_config.aws_secret_access_key)
+                        aws_access_key_id=awsKey.aws_access_key_id,
+                        aws_secret_access_key=awsKey.aws_secret_access_key)
     now=datetime.datetime.utcnow()
     for i in range (30,1,-1):
         startTime = now - datetime.timedelta(minutes=i)
@@ -132,70 +132,7 @@ def getAggregateStat30Mins(instances: list):
     missRate,
     hitRate]
           
-# def grep_vpc_subnet_id():
-#     ec2_client = boto3.client('ec2',
-#                             "us-east-1",
-#                             aws_access_key_id=cloudwatch_config.aws_access_key_id,
-#                             aws_secret_access_key=cloudwatch_config.aws_secret_access_key)
-#     response = ec2_client.describe_vpcs()
-#     vpc_id = ""
-#     print(response)
-#     for vpc in response["Vpcs"]:
-#         if vpc["InstanceTenancy"].__contains__("default"):
-#             vpc_id = vpc["VpcId"]
-#             break
-#     print("The Default VPC : ", vpc_id)
-#     response = ec2_client.describe_subnets(
-#         Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
-#     subnet_id = response["Subnets"][0]["SubnetId"]
-#     print("The Default Subnet : ", subnet_id)
-#     return vpc_id, subnet_id
 
-# def create_security_group():
-#     ec2_client = boto3.client('ec2',
-#                             "us-east-1",
-#                             aws_access_key_id=cloudwatch_config.aws_access_key_id,
-#                             aws_secret_access_key=cloudwatch_config.aws_secret_access_key)
-#     sg_name = "memcache_security_group"
-#     try:
-#         vpc_id, subnet_id = grep_vpc_subnet_id()
-#         response = ec2_client.create_security_group(
-#             GroupName=sg_name,
-#             Description="Memcache Security Group. This is created using python",
-#             VpcId=vpc_id
-#         )
-#         sg_id = response["GroupId"]
-#         print(sg_id)
-#         sg_config = ec2_client.authorize_security_group_ingress(
-#             GroupId=sg_id,
-#             IpPermissions=[
-#                 {
-#                     'IpProtocol': 'tcp',
-#                     'FromPort': 22,
-#                     'ToPort': 22,
-#                     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-#                 }, {
-#                     'IpProtocol': 'tcp',
-#                     'FromPort': 5001,
-#                     'ToPort': 5001,
-#                     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-#                 }, {
-#                     'IpProtocol': 'tcp',
-#                     'FromPort': 80,
-#                     'ToPort': 80,
-#                     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
-#                 }
-#             ]
-#         )
-#         print(sg_config)
-#         return sg_id, sg_name
-#     except Exception as e:
-#         if str(e).__contains__("already exists"):
-#             response = ec2_client.describe_security_groups(GroupNames=[
-#                                                                 sg_name])
-#             sg_id = response["SecurityGroups"][0]["GroupId"]
-#             print(sg_id, sg_name)
-#             return sg_id, sg_name
         
 
     
