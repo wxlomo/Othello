35bc0a182c0c7283bf562cd879b9bd70abd3d670
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-11 09:16:34 -0500

    starting auto scaler

--- A_2/autoscaler/app/autoscaler.py
+++ A_2/autoscaler/app/autoscaler.py
@@ -1,20 +1,42 @@
 from flask import request
 from . import scaler
 import memfunc
-from memfunc import getAggregateMissRate1mins
-import threading
+from memfunc import getAggregateMissRate1mins, start_ec2_instance, stop_ec2_instance, num_running
 import time
+import requests
 
 missrate=0
+run=0
+MAXMISS=0.7
+MINMISS=0.3
+EXPAND=1
+SHRINK=1
+
 @scaler.before_first_request
-# create the thread to keep updating the statistics
-def threadedUpdate():
-    thread = threading.Thread(target=updatestat)
-    thread.start()
-
-
-# update the statistics in every 5 seconds
+# update every 1 mins
 def updatestat():
     while True:
         time.sleep(60)
-        getAggregateMissRate1mins
+        response=requests.get("http://localhost:5002/auto")
+        run=int(response.json())
+        response=requests.get("http://localhost:5002/expand")
+        EXPAND=int(response.json())
+        response=requests.get("http://localhost:5002/shrink")
+        SHRINK=int(response.json())
+        response=requests.get("http://localhost:5002/max")
+        MAXMISS=int(response.json())
+        response=requests.get("http://localhost:5002/min")
+        MINMISS=int(response.json())
+        if (run):
+            missrate=getAggregateMissRate1mins()
+            num=num_running()
+            if missrate>MAXMISS:
+                new=max(8,int(num*EXPAND))
+                add=new-num
+                for i in range(add):
+                    start_ec2_instance()
+            elif missrate<MINMISS:
+                new=max(1,int(num*SHRINK))
+                add=num-new
+                for i in range(add):
+                    stop_ec2_instance()
