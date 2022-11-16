7e037e22fa43f819064cd2d4cc7c8c64ae63fa17
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-16 03:10:50 -0500

    add userdata for launch memcache on startup

--- A_2/start.sh
+++ A_2/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd frontend && gunicorn --bind 0.0.0.0:5000 --workers 3 --worker-class gevent run:front&
 cd managerapp && gunicorn --bind 0.0.0.0:5002 --workers 1 run:manager&
-cd autoscaler && gunicorn --bind 0.0.0.0:5002 --workers 1 run:scaler
+cd autoscaler && gunicorn --bind 0.0.0.0:5003 --workers 1 run:scaler
