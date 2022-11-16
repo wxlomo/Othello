7f63c2f3cea926845fc4f6dd0cf92c9b5a7e33d0
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-16 06:50:56 -0500

    add userdata for launch memcache on startup

--- A_2/managerapp/app/managerfunc.py
+++ A_2/managerapp/app/managerfunc.py
@@ -262,8 +262,8 @@ def getAggregateStat30Mins():
                         aws_secret_access_key=awsKey['aws_secret_access_key'])
     now=datetime.datetime.utcnow()
     for j in range (30,1,-1):
-        startTime = now - datetime.timedelta(minutes=i)
-        endTime = now - datetime.timedelta(minutes=i-1)
+        startTime = now - datetime.timedelta(minutes=j)
+        endTime = now - datetime.timedelta(minutes=j-1)
         miss = 0
         total= 0
         numItem=0
