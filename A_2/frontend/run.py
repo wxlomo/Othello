a4f5983490a96185dd0a6c20c36b3719043c3b98
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-16 06:46:11 -0500

    add userdata for launch memcache on startup

--- A_2/frontend/run.py
+++ A_2/frontend/run.py
@@ -3,9 +3,9 @@ import boto3
 from app import front, config
 
 
-s3 = boto3.client('s3')
-s3.create_bucket(Bucket=config.s3_config['name'],
-                 CreateBucketConfiguration={'LocationConstraint': config.s3_config['region_name']})
+# s3 = boto3.client('s3')
+# s3.create_bucket(Bucket=config.s3_config['name'],
+#                  CreateBucketConfiguration={'LocationConstraint': config.s3_config['region_name']})
 if __name__ == '__main__':
     front.run('0.0.0.0', 5000, debug=False, threaded=True)
 
