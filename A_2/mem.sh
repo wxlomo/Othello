148dec79a5a2950cf64cb0065c40dba8fbd6e88d
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-16 19:43:31 -0500

    add userdata for launch memcache on startup

--- A_2/mem.sh
+++ A_2/mem.sh
@@ -1,4 +1,2 @@
-# #!/bin/bash
-# cd ~/ECE1779-Group9-Project-Code/A_2/memcache/runmemcache.py && gunicorn --bind 0.0.0.0:5001 --workers 1 run:mem
 
 python3 '/home/ubuntu/ECE1779-Group9-Project-Code/A_2/memcache/runmemcache.py'
