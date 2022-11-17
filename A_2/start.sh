96070f04485f2db95e50468dc56aa974be84f942
Haozhe <sunhaozhe.sun@mail.utoronto.ca>
2022-11-17 10:15:23 -0500

    add clear cache in delete all data

--- A_2/start.sh
+++ A_2/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash gunicorn --bind 0.0.0.0:5002 --workers 4 --worker-class gevent run:manager
+#!/bin/bash
 cd frontend && gunicorn --bind 0.0.0.0:5000 --workers 2 --worker-class gevent run:front&
 cd managerapp && python3 run.py&
 cd autoscaler && gunicorn --bind 0.0.0.0:5003 --workers 1 run:scaler
