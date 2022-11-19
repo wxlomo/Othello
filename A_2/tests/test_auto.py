2339d64fe73fcfeec523d8bf394d043d85cd6416
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-11-19 01:56:58 -0500

    update the autotest

--- A_2/tests/test_auto.py
+++ A_2/tests/test_auto.py
@@ -19,15 +19,15 @@ import seaborn as sns
 import pandas as pd
 from time import time, sleep
 from matplotlib import pyplot as plt
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from matplotlib.ticker import MultipleLocator
 
 
 # The http address regards the deployed web application
-base_url = "http://3.80.40.201:5000/"
+base_url = "http://52.54.105.207:5000/"
 # The http address regards the manager application
-manager_url = "http://3.80.40.201:5002/"
+manager_url = "http://52.54.105.207:5002/"
 
-key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+key = []
 sns.set_theme(style="whitegrid")
 
 
@@ -92,19 +92,18 @@ def key_pool(mode):
     print(str(mode) + 'ing the key pool')
     global key
     if mode == 'grow':
-        if len(key) < 30:
-            while True:
-                new_key = random.randint(1, 100)
-                if new_key not in key:
-                    key.append(new_key)
-                    write(new_key)
-                    break
+        while True:
+            new_key = random.randint(1, 10000)
+            if new_key not in key:
+                key.append(new_key)
+                write(new_key)
+                break
     elif mode == 'shrink':
         if len(key) > 1:
             key.pop()
     else:
         raise ValueError('No such type of test')
-    print('The key pool now has: ' + str(key))
+    print('The key pool now has ' + str(len(key)) + ' keys')
 
 
 def get_miss():
@@ -145,9 +144,17 @@ def test_auto(mode):
           n/a
     """
     data = []
-    for ckey in key:
-        write(ckey)
-        read(ckey)
+    data2 = []
+    previous_miss = 0.0
+    previous_key = 0
+    if mode == 'grow':
+        previous_node = 1
+        for i in range(4):
+            key_pool('grow')
+    else:
+        previous_node = 8
+        for i in range(150):
+            key_pool('grow')
     while True:
         read_test()
         read_test()
@@ -156,17 +163,51 @@ def test_auto(mode):
         read_test()
         current_node = get_node()
         current_miss = get_miss()
-        if mode == 'grow' and current_miss < 0.75:
+        if mode == 'grow' and current_miss <= 0.75:
             key_pool('grow')
-        elif mode == 'shrink' and current_miss > 0.15:
+        elif mode == 'shrink' and current_miss >= 0.15:
             key_pool('shrink')
-        data.append([current_node, current_miss * 100])
-        if current_node == 1 or current_node == 8:
+        current_key = len(key)
+        if current_node == previous_node and current_miss != previous_miss and current_key == previous_key:
+            if mode == 'grow':
+                current_key += 1
+            else:
+                current_key -= 1
+        if current_node != previous_node:
+            if current_node == 1 or current_node == 2 or current_node == 4 or current_node == 8:
+                data2.append([current_node, current_miss * 100])
+        if current_node != previous_node or current_miss != previous_miss:
+            previous_node = current_node
+            previous_miss = current_miss
+            previous_key = current_key
+            if current_node == 1 or current_node == 2 or current_node == 4 or current_node == 8:
+                data.append([current_key, current_node, current_miss * 100])
+        if mode == 'grow' and current_node == 8 and current_miss <= 0.75:
+            break
+        if mode == 'shrink' and current_node == 1 and current_miss >= 0.15:
+            break
+        if current_key == 200 or current_key == 1:
             break
         sleep(5)
-    df = pd.DataFrame(data, columns=['Number of nodes', 'Miss rate (%)'])
-    fig = sns.relplot(data=df, x='Number of nodes', y='Miss rate (%)', kind='line')
-    fig.fig.savefig('img/missrate' + str(mode) + '.png')
+    df = pd.DataFrame(data, columns=['Number of keys', 'Number of nodes', 'Miss rate (%)'])
+    df2 = pd.DataFrame(data2, columns=['Number of nodes after scale', 'Miss rate (%)'])
+    df.to_csv('img/auto-scalar-' + str(mode) + '.csv', encoding='utf-8')
+    df2.to_csv('img/auto-scalar-resize-' + str(mode) + '.csv', encoding='utf-8')
+    fig = sns.relplot(data=df, x='Number of keys', y='Miss rate (%)', kind='line', hue='Number of nodes', palette=sns.color_palette("tab10"), sort=False)
+    fig.axes[0][0].yaxis.set_major_locator(MultipleLocator(5))
+    sns.move_legend(fig, 'upper right', bbox_to_anchor=(.8, 1))
+    if mode == 'shrink':
+        for ax in fig.axes[0]:
+            ax.invert_xaxis()
+    fig.fig.savefig('img/auto-scalar-' + str(mode) + '.png')
+    fig2 = sns.relplot(data=df2, x='Number of nodes after scale', y='Miss rate (%)', kind='line', sort=False)
+    if mode == 'grow':
+        fig2.set(ylim=(70, 100))
+    else:
+        fig2.set(ylim=(0, 20))
+        for ax in fig2.axes[0]:
+            ax.invert_xaxis()
+    fig2.fig.savefig('img/auto-scalar-resize-' + str(mode) + '.png')
     print('DONE.')
 
 
