8979bb3283471d694214e5e0c24a0a6518fcc541
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-11-21 00:59:59 -0500

    update bash outside the folder

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd A_1/frontend && gunicorn --bind 0.0.0.0:5000 --workers 3 --worker-class gevent run:front&
-cd A_1/memcache && gunicorn --bind 0.0.0.0:5001 --workers 3 run:mem
+cd A_2/frontend && gunicorn --bind 0.0.0.0:5000 --workers 2 --worker-class gevent run:front&
+cd A_2/managerapp && python3 run.py&
+cd A_2/autoscaler && gunicorn --bind 0.0.0.0:5003 --workers 1 --timeout 300 run:scaler
