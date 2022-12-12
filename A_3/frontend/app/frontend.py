27271c6561086d70771a68d07bfa1ccc43bb914a
HaozheSun <1002482845@qq.com>
2022-12-12 01:37:45 -0500

    add game logic

--- A_3/frontend/app/frontend.py
+++ A_3/frontend/app/frontend.py
@@ -318,3 +318,80 @@ def board_render(game_id, player_name, disks):
 
 
 
+def isOnBoard(x, y):
+# Returns True if the coordinates are located on the board.
+  return x >= 0 and x <= 7 and y >= 0 and y <=7
+
+def isValidMove(board, tile, xstart, ystart):
+
+  # Returns False if the player's move on space xstart, ystart is invalid.
+
+  # If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
+
+  if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+      return False
+  board[xstart][ystart] = tile # temporarily set the tile on the board.
+
+  if tile == 'X':
+      otherTile = 'O'
+  else:
+      otherTile = 'X'
+
+  tilesToFlip = []
+  for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+      x, y = xstart, ystart
+      x += xdirection # first step in the direction
+      y += ydirection # first step in the direction
+      if isOnBoard(x, y) and board[x][y] == otherTile:
+          # There is a piece belonging to the other player next to our piece.
+          x += xdirection
+          y += ydirection
+          if not isOnBoard(x, y):
+              continue
+          while board[x][y] == otherTile:
+              x += xdirection
+              y += ydirection
+              if not isOnBoard(x, y): # break out of while loop, then continue in for loop
+                  break
+          if not isOnBoard(x, y):
+              continue
+          if board[x][y] == tile:
+              # There are pieces to flip over. Go in the reverse direction until we reach the original space, noting all the tiles along the way.
+              while True:
+                  x -= xdirection
+                  y -= ydirection
+                  if x == xstart and y == ystart:
+                      break
+                  tilesToFlip.append([x, y])
+
+
+
+  board[xstart][ystart] = ' ' # restore the empty space
+
+  if len(tilesToFlip) == 0: # If no tiles were flipped, this is not a valid move.
+
+      return False
+
+  return tilesToFlip
+
+def getValidMoves(board, tile):
+
+ # Returns a list of [x,y] lists of valid moves for the given player on the given board.
+  validMoves = []
+  for x in range(8):
+    for y in range(8):
+      if isValidMove(board, tile, x, y) != False:
+        validMoves.append([x, y])
+  return validMoves
+
+def getBoardWithValidMoves(board, tile):
+  # Returns a new board with . marking the valid moves the given player can make.
+  dupeBoard = []
+  for i in range(8):
+    dupeBoard.append([' '] * 8)
+  for x in range(8):
+    for y in range(8):
+      dupeBoard[x][y] = board[x][y]
+  for x, y in getValidMoves(dupeBoard, tile):
+      dupeBoard[x][y] = '.'
+  return dupeBoard
