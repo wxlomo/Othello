add567c5d86f0a315c1b0feabf9b8c990bdb6779
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-12-14 04:36:01 -0500

    fixed a typo

--- A_3/frontend/app/frontend.py
+++ A_3/frontend/app/frontend.py
@@ -5,9 +5,104 @@
  * Author: Weixuan Yang
  * Date: Dec. 11, 2022
 """
-from . import front, dynamodb as ddb, games_table, rank_bucket
-from flask import render_template, request, redirect, escape, jsonify
+from . import front, dynamodb as ddb, dynamodb_client, db, s3_client, s3
+from flask import render_template, request, redirect, escape, jsonify, g
 from uuid import uuid4
+
+
+def get_db():
+    """Get the game table.
+    Args:
+      n/a
+    Returns:
+      dynamoDB table object
+    """
+    if 'db' not in g:
+        if 'Games' not in dynamodb_client.list_tables()['TableNames']:
+            g.games_table = db.create_table(TableName="Games",
+                                            KeySchema=[
+                                                {'AttributeName': 'GameId', 'KeyType': 'HASH'}
+                                            ],
+                                            AttributeDefinitions=[
+                                                {'AttributeName': 'GameId', 'AttributeType': 'S'},
+                                                {'AttributeName': 'FoeId', 'AttributeType': 'S'},
+                                                {'AttributeName': 'HostId', 'AttributeType': 'S'},
+                                                {'AttributeName': 'Statusnow', 'AttributeType': 'S'}
+                                            ],
+                                            ProvisionedThroughput={
+                                                'ReadCapacityUnits': 1,
+                                                'WriteCapacityUnits': 1
+                                            },
+                                            GlobalSecondaryIndexes=[
+                                                {
+                                                    'IndexName': 'FoeId',
+                                                    'KeySchema': [
+                                                        {
+                                                            'AttributeName': 'FoeId',
+                                                            'KeyType': 'HASH',
+
+                                                        },
+
+                                                        {
+                                                            'AttributeName': 'Statusnow',
+                                                            'KeyType': 'RANGE',
+                                                        }
+
+                                                    ],
+                                                    'Projection': {
+                                                        'ProjectionType': 'ALL',
+
+                                                    },
+                                                    'ProvisionedThroughput': {
+                                                        'ReadCapacityUnits': 1,
+                                                        'WriteCapacityUnits': 1
+                                                    }
+                                                },
+                                                {
+                                                    'IndexName': 'HostId',
+                                                    'KeySchema': [
+                                                        {
+                                                            'AttributeName': 'HostId',
+                                                            'KeyType': 'HASH',
+
+                                                        },
+
+                                                        {
+                                                            'AttributeName': 'Statusnow',
+                                                            'KeyType': 'RANGE',
+                                                        }
+                                                    ],
+                                                    'Projection': {
+                                                        'ProjectionType': 'ALL',
+
+                                                    },
+                                                    'ProvisionedThroughput': {
+                                                        'ReadCapacityUnits': 1,
+                                                        'WriteCapacityUnits': 1
+                                                    }
+                                                }
+                                            ]
+                                            )
+            g.games_table.wait_until_exists()
+        else:
+            g.games_table = db.Table('Games')
+    return g.games_table
+
+
+def get_bucket():
+    """Get the s3 bucket.
+    Args:
+      n/a
+    Returns:
+      s3 bucket object
+    """
+    if 's3' not in g:
+        if 'Games' not in s3_client.list_buckets()['Buckets']['Name']:
+            g.rank_bucket = s3.create_bucket(Bucket='Games')
+            g.rank_bucket.wait_until_exists()
+        else:
+            g.rank_bucket = s3.Bucket(name='Games')
+    return g.rank_bucket
 
 
 @front.route('/')
@@ -47,7 +142,7 @@ def get_join():
       str: the arguments for the Jinja template
     """
     all_hosts = []
-    all_items = ddb.get_invites('None', games_table)
+    all_items = ddb.get_invites('None', get_db())
     for i in all_items:
         all_hosts.append(str(i['HostId']))
         front.logger.debug('\n* Current pending games: ' + str(i['GameId']))
@@ -79,7 +174,7 @@ def get_rank():
     """
     rank = []
     try:
-        for obj in rank_bucket.objects.all():
+        for obj in get_bucket().objects.all():
             rank.append([obj.key, obj.body])
     except Exception as error:
         front.logger.debug('\n* Error: ' + str(error))
@@ -114,11 +209,12 @@ def create_game():
     """
     player_name = escape(request.form['player_name'].strip())
     if not player_name or player_name == 'None' or player_name == 'draw':
-        return render_template('result.html', title='Invalid Player Name', message='Do not use spaces/None/draw as your player name')
+        return render_template('result.html', title='Invalid Player Name',
+                               message='Do not use spaces/None/draw as your player name')
     tile = request.form['player_side']
     front.logger.debug('\n* Creating a game with name: ' + str(player_name))
     game_id = str(uuid4())
-    response = ddb.create_new_game(game_id, str(player_name), 'None', tile, games_table)
+    response = ddb.create_new_game(game_id, str(player_name), 'None', tile, get_db())
     front.logger.debug(str(response))
     return redirect('/game/' + str(game_id) + '/' + str(player_name))
 
@@ -135,16 +231,17 @@ def join_game():
     """
     player_name = escape(request.form['player_name'].strip())
     if not player_name or player_name == 'None' or player_name == 'draw':
-        return render_template('result.html', title='Invalid Player Name', message='Do not use spaces as your player name')
+        return render_template('result.html', title='Invalid Player Name',
+                               message='Do not use spaces as your player name')
     host_name = request.form['host_name']
     front.logger.debug(
         '\n* Joining a game with name: ' + str(player_name) + ' and host name: ' + str(host_name))
-    game_data = ddb.get_games_status(host_name, "Pending", games_table)
+    game_data = ddb.get_games_status(host_name, "Pending", get_db())
     front.logger.debug(str(game_data))
     if not game_data:
         return render_template('result.html', title='Fail to Join the Game',
                                message='The game you want to join does not exist, please try again.')
-    response = ddb.join_existed_game(game_data, games_table, str(player_name))
+    response = ddb.join_existed_game(game_data, get_db(), str(player_name))
     front.logger.debug(str(response))
     if response == 'Not a valid game':
         return render_template('result.html', title='Fail to Join the Game',
@@ -165,10 +262,11 @@ def game(game_id, player_name):
     """
     if not player_name or not game_id:
         return render_template('result.html', title='403 Forbidden', message='This page is not reachable.')
-    game_data = ddb.get(game_id, games_table)
+    game_data = ddb.get(game_id, get_db())
     front.logger.debug(str(game_data))
     if not game_data:
-        return render_template('result.html', title='500 Internal Server Error', message='Failed to render the game board.')
+        return render_template('result.html', title='500 Internal Server Error',
+                               message='Failed to render the game board.')
     status = game_data["Statusnow"]
     if status == 'Finished':
         return refresh(game_id, player_name)
@@ -176,7 +274,7 @@ def game(game_id, player_name):
     foe_name = game_data['FoeId']
     if player_name == foe_name:
         foe_name = game_data['HostId']
-    
+
     if not foe_name or foe_name == 'None':  # the foe does not come in
         board = board_render(game_id, player_name, game_board, [])
         front.logger.debug('\n* Current game board: ' + str(board) + ', current foe name: ' + str(foe_name))
@@ -195,7 +293,7 @@ def game(game_id, player_name):
             if not moves:  # impossible to move
                 message = 'No valid move!'
                 if get_valid_moves(game_board, other_tile):
-                    ddb.update_turn(game_data, [], player_name, games_table)
+                    ddb.update_turn(game_data, [], player_name, get_db())
                 else:
                     player_score = ddb.count_disks(game_data, tile)
                     foe_score = ddb.count_disks(game_data, other_tile)
@@ -205,16 +303,17 @@ def game(game_id, player_name):
                     else:
                         foe_name = game_data['HostId']
                     if player_score > foe_score:  # player wins
-                        ddb.finish_game(game_data, games_table, player_name)
+                        ddb.finish_game(game_data, get_db(), player_name)
                     elif player_score < foe_score:  # foe wins
-                        ddb.finish_game(game_data, games_table, foe_name)
+                        ddb.finish_game(game_data, get_db(), foe_name)
                     else:  # draw
-                        ddb.finish_game(game_data, games_table, 'draw')
+                        ddb.finish_game(game_data, get_db(), 'draw')
             board = board_render(game_id, player_name, game_board, moves)
         else:
             message = 'Now it is your foe ' + str(foe_name) + "'s turn!"
             board = board_render(game_id, player_name, game_board, [])
-    return render_template('game.html', board=board, surr='/game/' + str(game_id)+'/' + str(player_name) + '/surrender', message=message)
+    return render_template('game.html', board=board,
+                           surr='/game/' + str(game_id) + '/' + str(player_name) + '/surrender', message=message)
 
 
 @front.route('/game/<game_id>/<player_name>/move/<loc>', methods=['POST'])
@@ -231,10 +330,11 @@ def move(game_id, player_name, loc):
     """
     if not player_name or not game_id or not loc:
         return render_template('result.html', title='403 Forbidden', message='This page is not reachable.')
-    game_data = ddb.get(game_id, games_table)
+    game_data = ddb.get(game_id, get_db())
     front.logger.debug(str(game_data))
     if not game_data:
-        return render_template('result.html', title='500 Internal Server Error', message='Failed to render the game board.')
+        return render_template('result.html', title='500 Internal Server Error',
+                               message='Failed to render the game board.')
     game_board = ddb.make_board(game_data)
     x_start, y_start = loc[0], loc[1]
     if game_data['OUser'] == player_name:
@@ -247,7 +347,7 @@ def move(game_id, player_name, loc):
     position = [str(x_start) + str(y_start)]
     for p in valid:
         position.append(str(p[0]) + str(p[1]))
-    ddb.update_turn(game_data, position, player_name, games_table)
+    ddb.update_turn(game_data, position, player_name, get_db())
     front.logger.debug('\n* A move is made on game: ' + str(game_id) + ' at ' + str(loc))
     return redirect('/game/' + str(game_id) + '/' + str(player_name))
 
@@ -265,13 +365,13 @@ def surrender(game_id, player_name):
     """
     if not player_name or not game_id:
         return render_template('result.html', title='403 Forbidden', message='This page is not reachable.')
-    game_data = ddb.get(game_id, games_table)
+    game_data = ddb.get(game_id, get_db())
     front.logger.debug(str(game_data))
     if player_name == game_data['HostId']:
         winner = game_data['FoeId']
     else:
         winner = game_data['HostId']
-    ddb.finish_game(game_data, games_table, winner)
+    ddb.finish_game(game_data, get_db(), winner)
     front.logger.debug('\n* A player with name' + str(player_name) + ' surrender in game ' + str(game_id))
     return render_template('result.html', title='You Lose :(', message='Sorry to hear your leave.')
 
@@ -285,7 +385,7 @@ def data(game_id):
     Returns a JSON representation of the game to support AJAX to poll to see
     if the page should be refreshed
     """
-    game_data = ddb.get(game_id, games_table)
+    game_data = ddb.get(game_id, get_db())
     status = game_data["Statusnow"]
     turn = game_data["Turn"]
 
@@ -305,7 +405,7 @@ def refresh(game_id, player_name):
     """
     if not player_name or not game_id:
         return render_template('result.html', title='403 Forbidden', message='This page is not reachable.')
-    game_data = ddb.get(game_id, games_table)
+    game_data = ddb.get(game_id, get_db())
     front.logger.debug(str(game_data))
     status = game_data["Statusnow"]
     if status == 'Finished':
@@ -316,11 +416,11 @@ def refresh(game_id, player_name):
         player_score = ddb.count_disks(game_data, tile)
         front.logger.debug('\n* The player' + str(player_name) + ' has score ' + str(player_score))
         winner = game_data['Winner']
-        # response = ddb.teardown(game_id, games_table)
+        # response = ddb.teardown(game_id, get_db())
         # front.logger.debug(str(response))
         if winner == player_name:
             try:
-                rank_bucket.put_object(Key=winner, Body=player_score)
+                get_bucket().put_object(Key=winner, Body=player_score)
             except Exception as error:
                 front.logger.debug('\n* Error: ' + str(error))
                 return render_template('result.html', title='500 Internal Server Error',
@@ -331,7 +431,8 @@ def refresh(game_id, player_name):
             return render_template('result.html', title='You Lose :(',
                                    message='Your final score is ' + str(player_score) + '.')
         else:
-            return render_template('result.html', title='Draw...', message='Your final score is ' + str(player_score) + '.')
+            return render_template('result.html', title='Draw...',
+                                   message='Your final score is ' + str(player_score) + '.')
     else:
         front.logger.debug('\n* Refreshing the game board')
         return redirect('/game/' + str(game_id) + '/' + str(player_name))
@@ -419,7 +520,7 @@ def valid_move(board, tile, x, y):
                     y_start -= y_direction
                     if x_start == x and y_start == y:
                         break
-                    
+
                     tiles_to_flip.append([x_start, y_start])
     board[x][y] = ' '  # restore the empty space
     return tiles_to_flip
