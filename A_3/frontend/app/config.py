a85d7283a443dad203ca3d4715b835864c78e0f1
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-12-18 02:16:49 -0500

    reverse the aws credentials

--- A_3/frontend/app/config.py
+++ A_3/frontend/app/config.py
@@ -1,5 +1,5 @@
 aws_key = {
-    'aws_access_key_id': 'AKIAQOKHZVN4QZPD3CNT',
-    'aws_secret_access_key': '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',
+    'aws_access_key_id': 'AKIAZWOGCM7TACOR4IEW',
+    'aws_secret_access_key': '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',
     'aws_region': 'us-east-1'
 }
