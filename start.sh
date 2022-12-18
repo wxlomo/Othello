3922ef580adc986e482abd5517cbc6c562c0e0c7
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-12-18 04:05:48 -0500

    update the .sh file

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd A_2/frontend && gunicorn --bind 0.0.0.0:5000 --workers 2 --worker-class gevent run:front&
-cd A_2/managerapp && python3 run.py&
-cd A_2/autoscaler && gunicorn --bind 0.0.0.0:5003 --workers 1 --timeout 300 run:scaler
+cd A_3/frontend && gunicorn --bind 127.0.0.1:5000 --workers 3 run:front
